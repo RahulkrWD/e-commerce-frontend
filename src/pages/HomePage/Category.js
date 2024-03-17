215e2ef32d49537ee775aff2f6d6c84f3b96ccdc
RahulkrWD <rahulkmrgaya21@gmail.com>
2024-03-17 16:13:12 +0530

    update category

--- src/pages/HomePage/Category.js
+++ src/pages/HomePage/Category.js
@@ -1,56 +1,22 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import styles from "./HomePage.module.css";
 import { Link } from "react-router-dom";
-import Skeleton from "@mui/material/Skeleton";
-import Box from "@mui/material/Box";
-import Loading from "../../components/layout/Loading";
+import category from "../../jsonData/category.json";
 
 function Category() {
-  const [category, setCategory] = useState();
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    async function fetchCategory() {
-      try {
-        const response = await axios.get(
-          `${process.env.REACT_APP_API}/product/category`
-        );
-        setCategory(response.data);
-        setLoading(false);
-      } catch (err) {
-        setCategory("");
-        setLoading(false);
-      }
-    }
-    fetchCategory();
-  });
-
   return (
     <>
       <div className={`d-flex p-3 gap-4 ${styles.container}`}>
-        {loading ? (
-          <Box sx={{ width: 300 }}>
-            <Skeleton />
-            <Skeleton animation="wave" />
-            <Skeleton animation={false} />
-          </Box>
-        ) : category ? (
-          category.map((data, index) => (
-            <Link
-              key={index}
-              to={`/category?${data.categoryName}=${data.categoryId}`}
-              style={{ textDecoration: "none" }}
-            >
-              <img className={styles.categoryImage} src={data.image} alt="" />
-              <h5 className={styles.categoryName}>{data.categoryName}</h5>
-            </Link>
-          ))
-        ) : (
-          <center className="p-5">
-            <Loading />
-          </center>
-        )}
+        {category.map((data, index) => (
+          <Link
+            key={index}
+            to={`/category?${data.categoryName}=${data.categoryId}`}
+            style={{ textDecoration: "none" }}
+          >
+            <img className={styles.categoryImage} src={data.image} alt="" />
+            <h5 className={styles.categoryName}>{data.categoryName}</h5>
+          </Link>
+        ))}
       </div>
     </>
   );
--- src/pages/HomePage/Deal99.js
+++ src/pages/HomePage/Deal99.js
@@ -4,6 +4,7 @@ import Rating from "@mui/material/Rating";
 import StarIcon from "@mui/icons-material/Star";
 import styles from "./Deals.module.css";
 import womenDeals from "../../jsonData/womenDeals.json";
+import Kitchen from "../../jsonData/kitchen.json";
 import { Link } from "react-router-dom";
 function Deal99() {
   return (
@@ -12,9 +13,13 @@ function Deal99() {
       <h6>Mens Deals</h6>
       <div className={styles.mens_deals}>
         {mensDeals.map((items, index) => (
-          <Link key={index} className={styles.card}>
+          <Link
+            key={index}
+            className={styles.card}
+            to={`/category?${items.categoryName}=${items.categoryId}`}
+          >
             <img src={items.image} className={` ${styles.image}`} alt="" />
-            <div className="">
+            <div className="details">
               <h5 className="card-title text-center p-2">{items.type}</h5>
               <Rating
                 style={{ fontSize: "14px" }}
@@ -33,9 +38,13 @@ function Deal99() {
       <h6>Womens deals</h6>
       <div className={styles.womens_deals}>
         {womenDeals.map((items, index) => (
-          <Link key={index} className={styles.card}>
+          <Link
+            key={index}
+            className={styles.card}
+            to={`/category?${items.categoryName}=${items.categoryId}`}
+          >
             <img src={items.image} className={` ${styles.image}`} alt="" />
-            <div className="">
+            <div className="details">
               <h5 className="card-title text-center p-2">{items.type}</h5>
               <Rating
                 style={{ fontSize: "14px" }}
@@ -50,6 +59,38 @@ function Deal99() {
             </div>
           </Link>
         ))}
+      </div>
+      <div className={styles.coupons}>
+        <div className="col d-flex flex-wrap">
+          {Kitchen.map((items, index) => (
+            <Link key={index} className={styles.card}>
+              <img
+                src={items.image}
+                className={` ${styles.kitchen_image}`}
+                alt=""
+              />
+              <div className="details">
+                <Rating
+                  style={{ fontSize: "14px" }}
+                  name="text-feedback"
+                  readOnly
+                  precision={0.5}
+                  value={items.rating}
+                  emptyIcon={
+                    <StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />
+                  }
+                />
+              </div>
+            </Link>
+          ))}
+        </div>
+        <div className="coupons col w-25">
+          <img
+            style={{ width: "300px", height: "400px" }}
+            src="https://i.ibb.co/pPcW0d8/100rs-off.jpg"
+            alt=""
+          />
+        </div>
       </div>
     </div>
   );
